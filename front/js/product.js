f25faa58c668963ad3dc595a782f873007034507
AngeliqueMarquis <75800488+AngeliqueMarquis@users.noreply.github.com>
2022-01-18 16:42:14 +0100

    Added product.js file

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,20 @@
-import Product from "./models/Product.js"
+/* Récupération de ID des articles */
+const str = window.location.href;
+var url = new URL(str);
+const id = url.searchParams.get("id");
+console.log(id);
 
-var text = document.getElementById("item").innerText;
-      alert(text);
-      console.log();
+/* Récupération des données des articles */
+const urlProduct = fetch (`http://localhost:3000/api/products/${id}`);
+
+urlProduct
+    .then((response) => response.json())
+    .then ((itemsData)=> {
+    const picture = document.querySelector(".item__img");
+    const itemsPicture = document.createElement("img");
+    itemsPicture.src = itemsData.imageUrl;
+    picture.appendChild(itemsPicture);
+
+    
+    
+    });
